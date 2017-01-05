1854b3cb189e6a44f0207a9e6a124dfe480144a3
Henny Sipma <hennysipma@mac.com>
2017-01-05 12:00:15 -0800

    file and application objects

---
+++ advance/app/CApplication.py
@@ -0,0 +1,154 @@
+# ------------------------------------------------------------------------------
+# Access to the C Analyzer Analysis Results
+# Author: Henny Sipma
+# ------------------------------------------------------------------------------
+# The MIT License (MIT)
+#
+# Copyright (c) 2017 Kestrel Technology LLC
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ------------------------------------------------------------------------------
+import os
+
+import advance.util.fileutil as UF
+
+from advance.app.CCompInfo import CCompInfo
+from advance.app.CFile import CFile
+from advance.app.CVarInfo import CVarInfo
+from advance.source.CSrcFile import CSrcFile
+
+class CApplication():
+    '''Primary access point for source code and analysis results.'''
+
+    def __init__(self,path,appname):
+        self.path = os.path.join(path,'ktadvance')
+        self.srcpath = os.path.join(path,'sourcefiles')
+        self.appname = appname
+        self.xnode = UF.get_linkfile_xnode(self.path,self.appname)
+        self.filenames = {}          # file index -> filename
+        self.files = {}              # filename -> CFile
+        self.compinfos = {}          # ckey -> CCompInfo 
+        self.varinfos = {}           # vid -> CVarInfo         
+        self._initialize()
+
+    def getfilenames(self): return self.filenames.values()
+
+    def getfiles(self):
+		self._initialize_files()
+		return self.files.values()
+
+    def getfile(self,fname):
+        index = self.getfileindex(fname)
+        self._initialize_file(index,fname)
+        return self.files[fname]
+
+    def getfilebyindex(self,index):
+        if index in self.filenames:
+            return self.getfile(self.filenames[index])
+
+    def getfileindex(self,fname):
+        for i in self.filenames:
+            if self.filenames[i] == fname: return i
+            
+
+    def getsrcfile(self,fname):
+        return CSrcFile(self,os.path.join(self.srcpath,fname))
+
+    def fileiter(self,f):
+        for file in self.getfiles(): f(file)
+
+    def getexternals(self):
+		result = {}
+		for e in self.xnode.find('global-definitions').find('external-varinfos'):
+			vfile = e.get('vfile')
+			vname = e.get('vname')
+			summarized = e.get('summarized')
+			if vfile not in result: result[vfile] = []
+			result[vfile].append((vname,summarized))
+		return result
+
+    def getcompinfos(self):
+		self._initialize_compinfos()
+		return self.compinfos
+
+    def getcompinfo(self,fileindex,ckey):
+        return self.getfilebyindex(fileindex).getcompinfo(ckey)
+
+    def getfilecompinfos(self):
+        result = []
+        def f(f):result.extend(f.getcompinfos())
+        self.fileiter(f)
+        return result
+
+    def getvarinfos(self):
+		self._initialize_varinfos()
+		return self.varinfos.values()
+
+    '''
+
+    def get_ppo_results(self):
+        results = {}
+        def add(t,m,v):
+            if not t in results: results[t] = {}
+            if not m in results[t]: results[t][m] = 0
+            results[t][m] += v
+        def f(file): 
+            fileresults = file.get_ppo_results()
+            for tag in fileresults:
+                for m in fileresults[tag]:
+                    add(tag,m,fileresults[tag][m])
+        self.fileiter(f)
+        return results
+
+    '''
+        
+    def _initialize(self):
+        cfiles = self.xnode.find('c-files')
+        for c in cfiles.findall('c-file'):
+            id = c.get('id')
+            if id is None:
+                print('No id found for ' + c.get('name'))
+            else:
+                self.filenames[int(id)] = c.get('name')
+
+    def _initialize_compinfos(self):
+		if len(self.compinfos) > 0: return
+		cinfos = self.xnode.find('global-definitions').find('compinfos')
+		for c in cinfos.findall('compinfo'):
+			ckey = int(c.get('ckey'))
+			self.compinfos[ckey] = CCompInfo(self,c,hasglobalid=True)
+
+    def _initialize_varinfos(self):
+		if len(self.varinfos) > 0: return
+		vinfos = self.xnode.find('global-definitions').find('varinfos')
+		for v in vinfos.findall('varinfo'):
+			vid = int(v.get('vid'))
+			self.varinfos[vid] = CVarInfo(self,v)
+
+    def _initialize_files(self):
+		for i,f in self.filenames.items(): self._initialize_file(i,f)
+
+    def _initialize_file(self,index,fname):
+		if fname in self.files: return
+		cfile = UF.get_cfile_xnode(self.path,fname)
+		if not cfile is None:
+			self.files[fname] = CFile(self,index,cfile)
+		
+
+        
---
+++ advance/app/CFile.py
@@ -0,0 +1,225 @@
+# ------------------------------------------------------------------------------
+# Access to the C Analyzer Analysis Results
+# Author: Henny Sipma
+# ------------------------------------------------------------------------------
+# The MIT License (MIT)
+#
+# Copyright (c) 2017 Kestrel Technology LLC
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ------------------------------------------------------------------------------
+
+import advance.util.fileutil as UF
+
+from advance.app.CFunction import CFunction
+from advance.app.CGCompTag import CGCompTag
+from advance.app.CGEnumTag import CGEnumTag
+from advance.app.CGFunction import CGFunction
+from advance.app.CGType import CGType
+from advance.app.CGVarDecl import CGVarDecl
+from advance.app.CGVarDef import CGVarDef
+
+class CFile():
+    '''C File level declarations.'''
+
+    def __init__(self,capp,index,xnode):
+        self.index = index
+        self.capp = capp
+        self.xnode = xnode
+        self.gtypes = {}            # name -> CGType
+        self.gcomptagdefs = {}      # key -> CGCompTag
+        self.gcomptagdecls = {}     # key -> CGCompTag
+        self.gvardecls = {}         # vid -> CGVarDecl
+        self.gvardefs = {}          # vid -> CGVarDef
+        self.genumtagdefs = {}      # ename -> CGEnumTag
+        self.genumtagdecls = {}     # ename -> CGEnumTag
+        self.gfunctions = {}        # vid -> CGFunction
+        self.functions = {}         # vid -> CFunction
+        self.functionnames = {}     # functionname -> vid
+
+    def getindex(self): return self.index
+
+    def getxrefs(self): return self.xrefs
+
+    def getfilename(self): return self.xnode.get('filename')
+
+    def getgtypes(self):
+        self._initialize_gtypes()
+        return self.gtypes.values()
+
+    def getgtype(self,name):
+        self._initialize_gtypes()
+        if name in self.gtypes:
+            return self.gtypes[name].gettypeinfo().gettype()
+
+    def getgcomptagdefs(self):
+        self._initialize_gcomptagdefs()
+        return self.gcomptagdefs.values()
+
+    def getgcomptagdecls(self):
+        self._initialize_gcomptagdecls()
+        return self.gcomptagdecls.values()
+
+    def getgcomptag(self,key):
+        self._initialize_gcomptagdefs()
+        self._initialize_gcomptagdecls()
+        if key in self.gcomptagdefs:
+            return self.gcomptagdefs[key]
+        if key in self.gcomptagdecls:
+            return self.gcomptagdecls[key]
+        print('Key ' + str(key) + ' not found in ' + self.getfilename())
+        for key in (self.gcomptagdefs.keys() + self.gcomptagdecls.keys()):
+            print(str(key))
+
+    def getcompinfos(self):
+        return [ c.getcompinfo() for c in (self.getgcomptagdecls() + self.getgcomptagdefs()) ]
+
+    def getcompinfo(self,key):
+        comptag = self.getgcomptag(key)
+        if not comptag is None:
+            return self.getgcomptag(key).getcompinfo()
+        print(self.getfilename() + ': comptag with index ' + str(key) + ' not found')
+
+    def getgenumtagdefs(self):
+        self._initialize_genumtagdefs()
+        return self.genumtagdefs.values()
+
+    def getgenumtagdecls(self):
+        self._initialize_genumtagdecls()
+        return self.genumtagdecls.values()
+
+    def getgvardecls(self):
+        self._initialize_gvardecls()
+        return self.gvardecls.values()
+
+    def getgvardefs(self):
+        self._initialize_gvardefs()
+        return self.gvardefs.values()
+
+    def getgfunctions(self):
+        self._initialize_gfunctions()
+        return self.gfunctions.values()
+
+    def getgfunction(self,vid):
+        self._initialize_gfunctions()
+        return self.gfunctions[vid]
+
+    def getfunctionbyname(self,fname):
+        self._initialize_functions()
+        if fname in self.functionnames:
+            vid = self.functionnames[fname]
+            return self.functions[vid]
+        else:
+            print('Function name ' + fname + ' not found in ' + self.getfilename())
+            print('Names: ' + str(self.functionnames.keys()))
+
+    def getfunctions(self):
+        self._initialize_functions()
+        return self.functions.values()
+
+    def fniter(self,f):
+        for fn in self.getfunctions(): f(fn)
+
+    '''
+
+    def get_ppo_results(self):
+        results = {}
+        def add(t,m,v):
+            if not t in results: results[t] = {}
+            if not m in results[t]: results[t][m] = 0
+            results[t][m] += v
+        def f(fn): 
+            fnresults = fn.get_ppo_results()
+            for tag in fnresults:
+                for m in fnresults[tag]:
+                    add(tag,m,fnresults[tag][m])
+        self.fniter(f)
+        return results
+
+    def get_open_ppos(self):
+        results = {}
+        def f(fn):
+            openppos = fn.get_open_ppos()
+            results[fn.getname()] = openppos
+        self.fniter(f)
+        return results
+
+    '''
+
+    def _initialize_gtypes(self):
+        if len(self.gtypes) > 0: return
+        for t in self.xnode.find('global-type-definitions').findall('gtype'):
+            name = t.find('typeinfo').get('tname')
+            self.gtypes[name] = CGType(self,t)
+
+    def _initialize_gcomptagdefs(self):
+        if len(self.gcomptagdefs) > 0: return
+        for c in self.xnode.find('global-comptag-definitions').findall('gcomptag'):
+            key = int(c.find('compinfo').get('ckey'))
+            self.gcomptagdefs[key] = CGCompTag(self,c)
+
+    def _initialize_gcomptagdecls(self):
+        if len(self.gcomptagdecls) > 0: return
+        for c in self.xnode.find('global-comptag-declarations').findall('gcomptagdecl'):
+            key = int(c.find('compinfo').get('ckey'))
+            self.gcomptagdecls[key] = CGCompTag(self,c)
+
+    def _initialize_genumtagdefs(self):
+        if len(self.genumtagdefs) > 0: return
+        for e in self.xnode.find('global-enumtag-definitions').findall('genumtag'):
+            name = e.find('enuminfo').get('ename')
+            self.genumtagdefs[name] = CGEnumTag(self,e)
+
+    def _initialize_genumtagdecls(self):
+        if len(self.genumtagdecls) > 0: return
+        for e in self.xnode.find('global-enumtag-declarations').findall('genumtag'):
+            name = e.find('enuminfo').get('ename')
+            self.genumtagdecls[name] = CGEnumTag(self,e)
+
+    def _initialize_gvardecls(self):
+        if len(self.gvardecls) > 0: return
+        for v in self.xnode.find('global-var-declarations').findall('gvardecl'):
+            vid = int(v.find('varinfo').get('vid'))
+            self.gvardecls[vid] = CGVarDecl(self,v)
+
+    def _initialize_gvardefs(self):
+        if len(self.gvardefs) > 0: return
+        for v in self.xnode.find('global-var-definitions').findall('gvar'):
+            vid = int(v.find('varinfo').get('vid'))
+            self.gvardefs[vid] = CGVarDef(self,v)
+
+    def _initialize_gfunctions(self):
+        if len(self.gfunctions) > 0: return
+        for f in self.xnode.find('functions').findall('gfun'):
+            vid = int(f.find('svar').get('vid'))
+            self.gfunctions[vid] = CGFunction(self,f)
+
+    def _initialize_function(self,vid):
+        if vid in self.functions: return
+        fname = self.getgfunction(vid).getname()
+        f = UF.get_cfun_xnode(self.capp.path,self.getfilename(),fname)
+        if not f is None:
+            self.functions[vid] = CFunction(self,f)
+            self.functionnames[fname] = vid
+
+    def _initialize_functions(self):
+        self._initialize_gfunctions()
+        for vid in self.gfunctions.keys():
+            self._initialize_function(vid)
+        
