65197fc30cab3b7fcc6f282f92c89a4ed2ae2188
Henny Sipma <hennysipma@mac.com>
2017-01-08 22:16:13 -0800

    preliminary script to show the status of ppos

---
+++ advance/reporting/ProofObligationResults.py
@@ -0,0 +1,72 @@
+# ------------------------------------------------------------------------------
+# Access to the C Analyzer Analysis Results
+# Author: Henny Sipma
+# ------------------------------------------------------------------------------
+# The MIT License (MIT)
+#
+# Copyright (c) 2016 Kestrel Technology LLC
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ------------------------------------------------------------------------------
+
+import advance.util.printutil as UP
+
+class ProofObligationResults():
+
+    def __init__(self,results):
+        self.results = results      # tag -> method -> count
+
+    def report(self):
+        taglen = 24
+        lines = []
+        def sz(s):
+            l = len(s)
+            if l > 5:return l
+            return 6
+        methods = self._getmethods()
+        lines.append((' ' * taglen) + '  '.join(UP.rjust(m,sz(m)) for m in methods) + 
+                     '     ' + 'total')
+        lines.append('-' * 80)
+        for tag in sorted(self.results):
+            linetotal = sum(self._getcount(tag,m) for m in methods)
+            line = '  '.join(UP.rjust(str(self._getcount(tag,m)),sz(m)) for m in methods)
+            lines.append(UP.ljust(tag,taglen) + line + '  ' + UP.rjust(str(linetotal),8))
+        lines.append('-' * 80)
+        mtotal = '  '.join(UP.rjust(str(sum(self._getcount(tag,m) 
+                                            for tag in self.results)),sz(m))
+                                            for m in methods)
+        total = sum(sum(self._getcount(tag,m) for m in self.results[tag]) for tag in self.results)
+        lines.append(UP.ljust('total',taglen) + mtotal + '  ' + UP.rjust(str(total),8))
+        return '\n'.join(lines)
+
+    def _getcount(self,tag,m):
+        if tag in self.results:
+            if m in self.results[tag]:
+                return self.results[tag][m]
+        return 0
+        
+
+    def _getmethods(self):
+        methods = []
+        for tag in self.results:
+            for m in self.results[tag]:
+                if not m in methods: methods.append(m)
+        return methods
+
+            
---
+++ advance/reporting/chc_report_ppo_status.py
@@ -0,0 +1,89 @@
+# ------------------------------------------------------------------------------
+# Access to the C Analyzer Analysis Results
+# Author: Henny Sipma
+# ------------------------------------------------------------------------------
+# The MIT License (MIT)
+#
+# Copyright (c) 2017 Kestrel Technology LLC
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ------------------------------------------------------------------------------
+
+import argparse
+
+import advance.util.printutil as UP
+
+from advance.app.CApplication import CApplication
+from advance.reporting.ProofObligationResults import ProofObligationResults
+
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path',help='directory that holds the analysis results')
+    parser.add_argument('--cfile',
+                        help='only show this file: relative filename of the c source file',
+                        default=None)
+    parser.add_argument('--cfunction',help='only show this function: name of function',
+                        default=None)
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+
+    args = parse()
+    capp = CApplication(args.path)
+
+    lines = []
+
+    if not args.cfile is None:
+        cfile = capp.getfile(args.cfile)
+        if not args.cfunction is None:
+            cfunction = cfile.getfunctionbyname(args.cfunction)
+            fline = cfunction.getlocation().getline()
+            lines.append(cfile.getsourceline(fline))
+            def fpo(p,ev):
+                global fline
+                line = p.getlocation().getline()
+                if line >= fline:
+                    lines.append('-' * 80)
+                    for n in range(fline,line+1):
+                        lines.append(str(cfile.getsourceline(n)).strip())
+                    lines.append('-' * 80)
+                fline = line+1
+                prefix = '   '
+                delegated = ''
+                if ev is None:
+                    lines.append('   ' + str(p))
+                else:
+                    d = ev.getdischargemethod()
+                    if d == 'invariants': prefix = 'II '
+                    if d == 'check-valid': prefix = 'CV '
+                    if ev.isdelegated(): delegated = '--' + ev.getassumptiontype() + '--'
+                    lines.append(prefix + str(p) + ': ' + delegated + ev.getevidence())
+            print(cfunction.getname() + ' in ' + cfile.getfilename())            
+            print(ProofObligationResults(cfunction.get_ppo_results()).report())
+            cfunction.getproofs().iterpposev(fpo)
+            print('\n'.join(lines))
+        else:
+            print(cfile.getfilename())
+            print(ProofObligationResults(cfile.get_ppo_results()).report())
+    else:
+        print(ProofObligationResults(capp.get_ppo_results()).report())
+
+
+    
