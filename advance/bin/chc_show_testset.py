dc191e479b4f588f63aa7e952a8a41bdaa24fd87
Henny Sipma <hennysipma@mac.com>
2017-03-18 23:42:44 -0700

    script to print a test spec

---
+++ advance/bin/chc_show_testset.py
@@ -0,0 +1,50 @@
+# ------------------------------------------------------------------------------
+# KT Advance C Source Code Analyzer
+# Author: Henny Sipma
+# ------------------------------------------------------------------------------
+# The MIT License (MIT)
+#
+# Copyright (c) 2017 Kestrel Technology LLC
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ------------------------------------------------------------------------------
+
+import argparse
+import json
+import os
+
+from advance.bin.TestSetRef import TestSetRef
+
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path',help='directory that holds test sets')
+    parser.add_argument('test',help='name of test directory')
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+
+    args = parse()
+    testpath = args.path
+    testname = args.test
+    cpath = os.path.join(os.path.abspath(testpath),testname)
+    testfilename = os.path.join(cpath,testname + '.json')
+    testsetref = TestSetRef(testfilename)
+    print(str(testsetref))
+        
