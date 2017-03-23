64933a24af05a0881132ee4b8207487bc32510e7
Henny Sipma <hennysipma@mac.com>
2017-03-23 14:46:30 -0700

    script to analyze a single file (replaces chc_analyze_onefile_application.py)

--- advance/bin/chc_analyze_file.py
+++ advance/bin/chc_analyze_file.py
@@ -33,13 +33,22 @@ from contextlib import contextmanager
 
 import advance.util.fileutil as UF
 
-from advance.app.CApplication import CApplication
+from advance.bin.Config import Config
+from advance.app.CFileApplication import CFileApplication
 from advance.bin.AnalysisManager import AnalysisManager
 
 def parse():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('path',help='directory that holds that analysis results')
-    parser.add_argument('cfile',help='c filename with relative path to path argument')
+    usage = ('\nCall with the name of the directory that holds the semantics directory and\n' +
+                 'the name of the c file.\n\n' +
+                 '  Example: python chc_analyze_file.py ../../tests/sard/kendra/id115Q id115.c\n\n')
+    description = ('Analyzes the semantics files for the given c source code file. It expects\n' +
+                       'that the file has already been parsed and that the semantics files are\n' +
+                       'available either in a subdirectory of the passed in path, called\n' +
+                       'semantics, or in a (gzipped) tar file semantics_linux.tar.gz or \n' +
+                       'semantics_linux.tar (or semantics_mac.tar.gz/semantics_mac.tar resp.)')
+    parser = argparse.ArgumentParser(usage=usage,description=description)
+    parser.add_argument('path',help='directory that holds the semantics directory for the c file')
+    parser.add_argument('cfile',help='c filename')
     args = parser.parse_args()
     return args
 
@@ -53,11 +62,41 @@ def timing(activity):
 
 if __name__ == '__main__':
     args = parse()
-    capp = CApplication(args.path)
-    path = capp.getpath()
+    filepath = os.path.abspath(args.path)
     cfilename = args.cfile
+    if not os.path.isdir(filepath):
+        print('*' * 80)
+        print('Directory ' + filepath + ' not found.')
+        print('*' * 80)
+        exit(1)
 
-    am = AnalysisManager(capp)
+    ismac = Config().platform == 'mac'
+    semdir = os.path.join(filepath,'semantics')
+    if not os.path.isdir(semdir):
+        success = UF.unpack_tar_file(filepath)
+        if not success:
+            print('*' * 80)
+            print('No file or directory found with semantics')
+            print('Expected to find a directory ' + semdir)
+            if ismac:
+                print('or a file semantics_mac.tar.gz or semantics_mac.tar in ' + filepath)
+            else:
+                print('or a file semantics_linux.tar.gz or semantics_linux.tar in ' + filepath)
+            print('*' * 80)
+            exit(1)
+
+    capp = CFileApplication(semdir,cfilename)
+    ktadvpath = capp.getpath()
+    xfilename = UF.get_cfile_filename(ktadvpath,cfilename)
+    if not os.path.isfile(xfilename):
+        print('*' * 80)
+        print('No semantics file found for ' + cfilename)
+        print('Expected to find the file ' + xfilename)
+        print('Please parse the file first with chc_parse_file.py or check the directory name.')
+        print('*' * 80)
+        exit(1)
+    
+    am = AnalysisManager(capp,onefile=True)
 
     with timing('creating primary proof obligations'):
         am.create_file_primaryproofobligations(cfilename)
