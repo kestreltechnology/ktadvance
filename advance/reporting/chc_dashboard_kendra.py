d021f53c7af91967e7c7f8380a37b4a339dc3a2c
Andrew McGraw <mcgraw@kestreltechnology.com>
2017-07-07 11:57:02 -0700

    Show spos and add option to show violations

--- advance/reporting/chc_dashboard_kendra.py
+++ advance/reporting/chc_dashboard_kendra.py
@@ -47,7 +47,8 @@ unknowns (ppo)      number of ppos not discharged
 unknowns (spo)      number of spos not discharged (N/A if there are no spos)
 
 '''
-
+    
+import argparse
 import json
 import os
 
@@ -56,6 +57,12 @@ from advance.bin.Config import Config
 kendra = os.path.join(os.path.join(Config().testdir,'sard'),'kendra')
 testcases = [ (i,'id' + str(i) + 'Q') for i in range(115,394,4) ]
 
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--predicates',help='Show violated predicates with location',action='store_true')
+    args = parser.parse_args()
+    return args
+
 def getppocount(spec):
     total = 0
     for f in spec["cfiles"]:
@@ -63,12 +70,27 @@ def getppocount(spec):
             total += len(spec["cfiles"][f]["functions"][ff]["ppos"])
     return total
 
+def getspocount(spec):
+    total = 0
+    for f in spec["cfiles"]:
+        for ff in spec["cfiles"][f]["functions"]:
+            if "spos" in spec["cfiles"][f]["functions"][ff]:
+                total += len(spec["cfiles"][f]["functions"][ff]["spos"])
+    return total
+
 def getppos(spec,f):
     ppos = []
-    for ff in spec['cfiles'][f]['functions']:
-        ppos.extend(spec['cfiles'][f]['functions'][ff]['ppos'])
+    for ff in spec["cfiles"][f]["functions"]:
+        ppos.extend(spec["cfiles"][f]["functions"][ff]["ppos"])
     return ppos
-                    
+                
+def getspos(spec, f):
+    spos = []
+    for ff in spec["cfiles"][f]["functions"]:
+        if "spos" in spec["cfiles"][f]["functions"][ff]:
+            spos.extend(spec["cfiles"][f]["functions"][ff]["spos"])
+    return spos
+    
 def getviolationpredicates(spec):
     predicates = set([])
     for f in spec['cfiles']:
@@ -78,27 +100,45 @@ def getviolationpredicates(spec):
                 predicates.add((ppo['predicate'],ppo['cfgctxt'],ppo['expctxt']))
     return predicates
                                    
-def getsafeproofperc(spec):
+def getsafeproofperc(spec, obligationtype):
     safetgtcount = 0
     safecount = 0
     violationcount = 0
     violationtgtcount = 0
     unknowntgtcount = 0
     for f in spec['cfiles']:
-        for ppo in getppos(spec,f):
-            if ppo['status'] == 'safe':
-                safetgtcount += 1
-                safecount += 1
-            elif ppo['status'] == 'violation':
-                violationtgtcount += 1
-                violationcount += 1
-            elif ppo['tgtstatus'] == 'safe':
-                safetgtcount += 1
-            elif ppo['tgtstatus'] == 'violation':
-                violationtgtcount += 1
-            else:
-                unknowntgtcount += 1
-    safepct = float(safecount)/float(safetgtcount) * 100.0
+        if obligationtype == 'ppo':
+            for ppo in getppos(spec,f):
+                if ppo['status'] == 'safe':
+                    safetgtcount += 1
+                    safecount += 1
+                elif ppo['status'] == 'violation':
+                    violationtgtcount += 1
+                    violationcount += 1
+                elif ppo['tgtstatus'] == 'safe':
+                    safetgtcount += 1
+                elif ppo['tgtstatus'] == 'violation':
+                    violationtgtcount += 1
+                else:
+                    unknowntgtcount += 1
+        elif obligationtype == 'spo':
+            for spo in getspos(spec, f):
+                if spo['status'] == 'safe':
+                    safetgtcount += 1
+                    safecount += 1
+                elif spo['status'] == 'violations':
+                    violationtgtcount += 1
+                    violationcount += 1
+                elif spo['tgtstatus'] == 'safe':
+                    safetgtcount += 1
+                elif spo['tgtstatus'] == 'violation':
+                    violationtgtcount += 1
+                else:
+                    unknowntgtcount += 1
+    if safetgtcount > 0:
+        safepct = float(safecount)/float(safetgtcount) * 100.0
+    else:
+        safepct = 0.0
     if violationtgtcount > 0:
         violationpct = float(violationcount)/float(violationtgtcount) * 100.0
     else:
@@ -106,35 +146,43 @@ def getsafeproofperc(spec):
     unknowns = '  -'
     if unknowntgtcount > 0:
         unknowns = str(unknowntgtcount).rjust(3)
-    return ('{:>6.1f}'.format(safepct) + '    ' + '{:>6.1f}'.format(violationpct) +
-                '    ' + unknowns)
+    return ('{:>10.1f}'.format(safepct) + '    ' + '{:>10.1f}'.format(violationpct) +
+                '     ' + unknowns + '   ')
 
-def testdata(i,t,spec):
+def testdata(printpredicates,i,t,spec):
     lines = []
     ppocount = getppocount(spec)
-    proofperc = getsafeproofperc(spec)
+    spocount = getspocount(spec)
+    ppoproofperc = getsafeproofperc(spec, "ppo")
+    spoproofperc = ''
+    if spocount > 0 : spoproofperc = getsafeproofperc(spec, "spo")
     violationpreds = list(getviolationpredicates(spec))
-    base = t + '  ' + str(ppocount).rjust(5) + '  '
-    if len(violationpreds) == 0:
-        lines.append(base + (' ' * 104) + proofperc)
-    else:
+    base = t + '  ' + str(ppocount).rjust(5) + '  ' + str(spocount).rjust(3) + ' '
+    if len(violationpreds) == 0 and printpredicates == True:
+        lines.append(base + (' ' * 104) + ppoproofperc + spoproofperc)
+    elif printpredicates == True:
         lines.append(base + violationpreds[0][0].ljust(24) +
                          violationpreds[0][1].ljust(40) +
-                         violationpreds[0][2].ljust(40) + proofperc)
+                         violationpreds[0][2].ljust(40) + ppoproofperc + spoproofperc)
         for v in violationpreds[1:]:
-            lines.append((' ' * 15) + v[0].ljust(24) + v[1].ljust(40) + v[2].ljust(40))
+            lines.append((' ' * 19) + v[0].ljust(24) + v[1].ljust(40) + v[2].ljust(40))
+    else:
+        lines.append(base + ppoproofperc + spoproofperc)
     return lines
 
 if __name__ == '__main__': 
+    args = parse()
 
-    print('testcase'.ljust(9) + 'ppos'.rjust(5) + '  ' + 'predicates'.ljust(24) +
-              'cfg-context'.ljust(40) + 'exp-context'.ljust(40) +
-              '%safe'.rjust(6) + '  %violation'.rjust(10) + '  unknowns')
+    header = 'testcase'.ljust(9) + 'ppos'.rjust(5) + 'spos'.rjust(5) + '  ' 
+    if args.predicates: header += 'predicates'.ljust(24) + 'cfg-context'.ljust(40) + 'exp-context'.ljust(40)
+    header += '%safe (ppos)'.rjust(10) + '  %violation'.rjust(10) + '  unknowns  '
+    header += '%safe (spos)'.rjust(10) + '  %violation'.rjust(10) + '  unknowns'
+    print header
     print('-' * 150)
     for (i,t) in testcases:
         specfilename = os.path.join(os.path.join(kendra, t),t + '.json')
         with open(specfilename) as fp:
             spec = json.load(fp)
-        for l in testdata(i,t,spec):
+        for l in testdata(args.predicates,i,t,spec):
             print(l)
         
