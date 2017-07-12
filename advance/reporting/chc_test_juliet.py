ab7cae35453b88e071680f1061af1a4d2d33d714
Henny Sipma <hennysipma@mac.com>
2017-07-11 23:20:42 -0400

    restructure presentation of juliet test results

--- advance/reporting/chc_test_juliet.py
+++ advance/reporting/chc_test_juliet.py
@@ -45,6 +45,21 @@ def parse():
     args = parser.parse_args()
     return args
 
+def keymatches(tppo,ppo):
+    if (tppo.getline() == ppo.getline() and
+            tppo.getpredicate() == ppo.getpredicatetag()):
+        if ((not tppo.hasexpctxt()) or
+            (tppo.hasexpctxt() and ppo.getcontextstrings()[1] == tppo.getexpctxt())):
+            if ((not tppo.hasvariablenames()) or
+                    (tppo.hasvariablenames() and
+                         any([ ppo.hasvariable(vname) for vname in tppo.getvariablenames()]))):
+                return True
+    return False
+
+violationcategories = [ 'reported', 'found-safe', 'found-deferred', 'unknown', 'other' ]
+safecontrolcategories = [ 'stmt-safe', 'safe', 'deferred', 'deadcode', 'unknown', 'other']
+
+
 if __name__ == '__main__':
 
     args = parse()
@@ -75,34 +90,71 @@ if __name__ == '__main__':
             results[filename] = {}
         testppos = julietppos[filename]
         cfile = capp.getfile(filename)
-        fileppos = cfile.get_line_ppos()
-        for tppo in testppos:
-            line = tppo.getline()
-            pred = tppo.getpredicate()
-            if line in fileppos:
-                if pred in fileppos[line]:
-                    fn = fileppos[line][pred]['function']
-                    if not fn in results[filename]: results[filename][fn] = []
-                    if len(fileppos[line][pred]['ppos']) == 1:
-                        results[filename][fn].append((tppo,fileppos[line][pred]['ppos'][0]))
-                    else:
-                        exctxt = tppo.getexpctxt()
-                        for ppo in fileppos[line][pred]['ppos']:
-                            if ppo.getcontextstrings()[1] == exctxt:
-                                results[filename][fn].append((tppo,ppo))
+
+        fileppos = cfile.get_ppos()
+        for fn in fileppos:
+            if not fn in results[filename]: results[filename][fn] = []
+            for ppo in fileppos[fn]:
+                for tppo in testppos:
+                    if keymatches(tppo,ppo):
+                        results[filename][fn].append((tppo,ppo))
+    testsummary = {}
+
+    def f(tindex,test):
+        testsummary[tindex] = {}
+        testsummary[tindex]['violations'] = {}
+        for c in violationcategories:
+            testsummary[tindex]['violations'][c] = 0
+        testsummary[tindex]['safe-controls'] = {}
+        for c in safecontrolcategories:
+            testsummary[tindex]['safe-controls'][c] = 0
+    testset.iter(f)
+
     for filename in sorted(results):
         print('\n' + filename)
         for fn in sorted(results[filename]):
-            print('\n  ' + fn)
-            for (tppo,ppo) in results[filename][fn]:
-                ev = ppo.getevidence()
-                if ev is None:
-                    evstr = '?'
-                else:
-                    evstr = ev.getdisplayprefix() + '  ' + ev.getevidence()
-                print('    ' + str(ppo.getline()).rjust(3) + '  ' +
-                          str(ppo.getid()) + ': ' + ppo.getpredicatetag().ljust(25) + evstr)
-                if (not ev is None) and ev.isdelegatedtoapi():
+            if len(results[filename][fn]) > 0:
+                print('\n  ' + fn)
+                for (tppo,ppo) in results[filename][fn]:
+                    tindex = tppo.gettest()
+                    summary = testsummary[tindex]
+                    ev = ppo.getevidence()
+                    if ev is None:
+                        evstr = '?'
+                        if tppo.isviolation():
+                            summary['violations']['unknown'] += 1
+                        else:
+                            summary['safe-controls']['unknown'] += 1
+                    else:
+                        evstr = ev.getdisplayprefix() + '  ' + ev.getevidence()
+                        d = ev.getdischargemethod()
+                        if tppo.isviolation():
+                            if ev.isviolation():
+                                summary['violations']['reported'] += 1
+                            elif d == 'local' or d == 'stmt':
+                                summary['violations']['found-safe'] += 1
+                            elif ev.isdelegated():
+                                summary['violations']['found-deferred'] += 1
+                            else:
+                                summary['violations']['other'] += 1
+                        else:
+                            if ev.isviolation():
+                                summary['safe-controls']['other'] += 1
+                            elif d == 'stmt':
+                                summary['safe-controls']['stmt-safe'] += 1
+                            elif d == 'local':
+                                summary['safe-controls']['safe'] += 1
+                            elif ev.isdelegated():
+                                summary['safe-controls']['deferred'] += 1
+                            elif ev.isdeadcode():
+                                summary['safe-controls']['deadcode'] += 1
+                            else:
+                                summary['safe-controls']['other'] += 1
+
+
+                    print('    ' + str(ppo.getline()).rjust(3) + '  ' +
+                            str(ppo.getid()) + ': ' + ppo.getpredicatetag().ljust(25) + evstr)
+                    if (not ev is None) and ev.isdelegatedtoapi():
                         cfile = capp.getfile(filename)
                         cfun = cfile.getfunctionbyname(fn)
                         callsites = capp.getcallsites(cfile.getindex(),cfun.getid())
@@ -120,4 +172,31 @@ if __name__ == '__main__':
                                               spo.getpredicatetag().ljust(25) + sevtxt)
                                 cs.iter(f)
 
+    totals = {}
+    totals['violations'] = {}
+    totals['safe-controls'] = {}
+    for c in violationcategories:
+        totals['violations'][c] = sum([testsummary[x]['violations'][c] for x in testsummary ])
+    for c in safecontrolcategories:
+        totals['safe-controls'][c] = sum([testsummary[x]['safe-controls'][c] for x in testsummary ])
+
+    print('\n\nSummary')
+    print('\n')
+    print('test              violations                    safe-controls')
+    print('         V    S    D    U    O                S    L    D    X    U    O')
+    print('-' * 80)
+    for tindex in sorted(testsummary):
+        sv = testsummary[tindex]['violations']
+        ss = testsummary[tindex]['safe-controls']
+        print(tindex.ljust(5) +
+                  ''.join([str(sv[c]).rjust(5) for c in violationcategories]) +
+                  '       |    ' +
+                  ''.join([str(ss[c]).rjust(5) for c in safecontrolcategories]))
+    print('-' * 80)
+    print('total' +
+              ''.join([str(totals['violations'][c]).rjust(5) for c in violationcategories]) +
+              '       |    ' +
+              ''.join([str(totals['safe-controls'][c]).rjust(5) for c in safecontrolcategories]))
+
+    UF.save_juliet_test_summary(args.juliettest,totals)
 
