5ee8e4ab6a6d3890cf1f7ee5bca352b0c235b502
Henny Sipma <hennysipma@mac.com>
2017-07-15 12:28:50 -0700

    add target type as selection criterium for ppo's

--- advance/reporting/JulietTestFileRef.py
+++ advance/reporting/JulietTestFileRef.py
@@ -102,9 +102,11 @@ class JulietPpo():
         self.expctxt = None
         self.cfgctxt = None
         self.variablename = None
+        self.targettype = None
         if 'E' in d: self.expctxt = d['E']
         if 'C' in d: self.cfgctxt = d['C']
         if 'V' in d: self.variablename = d['V']
+        if 'T' in d: self.targettype = d['T']
 
     def gettest(self): return self.testfileref.gettest()
 
@@ -118,11 +120,15 @@ class JulietPpo():
 
     def getvariablenames(self): return self.variablename
 
+    def gettargettype(self): return self.targettype
+
     def hasexpctxt(self): return not (self.expctxt is None)
 
     def hascfgctxt(self): return not (self.cfgctxt is None)
 
     def hasvariablenames(self): return not (self.variablename is None)
+
+    def hastargettype(self): return not (self.targettype is None)
 
     def __str__(self):
         ctxt = ''
