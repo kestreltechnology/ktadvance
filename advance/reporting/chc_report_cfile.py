894e5dfe1b3dab995a3ad88f147ef115ad9c7612
Henny Sipma <hennysipma@mac.com>
2017-07-16 17:25:30 -0700

    script to display analysis results for a single file

---
+++ advance/reporting/chc_report_cfile.py
@@ -0,0 +1,60 @@
+# ------------------------------------------------------------------------------
+# Access to the C Analyzer Analysis Results
+# Author: Henny Sipma
+# ------------------------------------------------------------------------------
+# The MIT License (MIT)
+#
+# Copyright (c) 2017 Kestrel Technology LLC
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ------------------------------------------------------------------------------
+
+import argparse
+import os
+
+import advance.util.printutil as UP
+import advance.util.fileutil as UF
+
+from advance.bin.Config import Config
+from advance.app.CApplication import CApplication
+from advance.reporting.ProofObligationDisplay import ProofObligationDisplay
+from advance.reporting.ProofObligationResults import ProofObligationResults
+
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path',help='path to directory that holds the semantics directory')
+    parser.add_argument('cfile',help='filename of c file')
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+
+    args = parse()
+    sempath = os.path.join(args.path, 'semantics')
+    cfapp = CApplication(sempath,args.cfile)
+    cfile = cfapp.getcfile()
+    def f(cfun):
+        d = ProofObligationDisplay(cfile,cfun);
+        print(d.showppos())
+        print(d.showspos())
+        print(cfun.getapi())
+    cfapp.getcfile().fniter(f)
+
+    print('\n\nSummary of proof obligations:')
+    print(ProofObligationResults(cfile.get_ppo_results()).report())
