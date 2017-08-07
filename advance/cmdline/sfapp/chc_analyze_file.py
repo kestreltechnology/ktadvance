27ea54ba2065226cf0ddc3ebf6282a482e6b4124
Henny Sipma <hennysipma@mac.com>
2017-08-06 17:29:04 -0700

    fix missing import

--- advance/cmdline/sfapp/chc_analyze_file.py
+++ advance/cmdline/sfapp/chc_analyze_file.py
@@ -32,6 +32,7 @@ import os
 from contextlib import contextmanager
 
 import advance.util.fileutil as UF
+import advance.util.printutil as UP
 
 from advance.util.Config import Config
 from advance.app.CApplication import CApplication
