763dcc74863992b187ead4688fd30b260c4dbe0d
Henny Sipma <hennysipma@mac.com>
2017-10-05 17:24:30 -0700

    remove references to pevs and sevs

--- advance/proof/CFunctionProofs.py
+++ advance/proof/CFunctionProofs.py
@@ -45,17 +45,11 @@ object.
   - ppos: CFunctionPPOs
           - id -> CFunctionPPO
 
-  - pevs: CFunctionPEVs
-          - id -> CFunctionPEV
-
   - spos: CFunctionSPOs
           - callsitespos: context -> CFunctionCallsiteSPOs
                                      - id -> CFunctionCallsiteSPO
           - returnsitespos: context -> CFunctionReturnsiteSPOs
                                        id -> CFunctionReturnsiteSPO
-
-  - sevs: CFunctionSEVs
-          - id -> CFunctionSEV
 '''
 
 class CFunctionProofs():
@@ -64,149 +58,96 @@ class CFunctionProofs():
         self.cfun = cfun
         self.cfile = self.cfun.cfile
         self.capp = self.cfile.capp
+        self.cfile.predicatedictionary.initialize()
         self.ppos = None           # CFunctionPPOs
         self.spos = None           # CFunctionSPOs
-        self.pevs = None           # CFunctionPEVs
-        self.sevs = None           # CFunctionSEVs
 
-    def getfunction(self): return self.cfun
+    def add_returnsite_postcondition(self,postcondition):
+        self._get_spos()
+        self.spos.add_returnsite_postcondition(postcondition)
 
-    def getfile(self): return self.cfun.getfile()
-
-    def addreturnsiteobligation(self,rv,fvid):
-        self._getspos()
-        self.spos.addreturnsiteobligation(rv,fvid)
-
-    def updatespos(self):
-        self._getspos()
+    def update_spos(self):
+        self._get_spos()
         self.spos.update()
 
-    def savespos(self):
+    def reset_spos(self): self.spos = None
+
+    def save_spos(self):
         cnode = ET.Element('function')
-        cnode.set('name',self.getfunction().getname())
-        self.spos.writexml(cnode)
-        self._savespos(cnode)
+        cnode.set('name',self.cfun.name)
+        self.spos.write_xml(cnode)
+        self._save_spos(cnode)
 
-    def getppo(self,id):
-        self._getppos()
-        return self.ppos.getppo(id)
+    def get_ppo(self,id):
+        self._get_ppos()
+        return self.ppos.get_ppo(id)
 
-    def getspo(self,id):
-        self._getspos()
-        return self.spos.getspo(id)
+    def get_spo(self,id):
+        self._get_spos()
+        return self.spos.get_spo(id)
 
-    def iterppos(self,f): 
-        self._getppos()
+    def iter_ppos(self,f): 
+        self._get_ppos()
         self.ppos.iter(f)
 
-    def iterspos(self,f):
-        self._getspos()
+    def iter_spos(self,f):
+        self._get_spos()
         self.spos.iter(f)
 
-    def iterpevs(self,f):
-        self._getpevs()
-        self.pevs.iter(f)
-
-    def itercallsites(self,f):
-        self._getspos()
-        self.spos.itercallsites(f)
+    def iter_callsites(self,f):
+        self._get_spos()
+        self.spos.iter_callsites(f)
 
     def get_ppos(self):
         result = []
         def f(ppo): result.append(ppo)
-        self.iterppos(f)
+        self.iter_ppos(f)
         return result
 
-    def get_spos(self):
+    def get_spos(self,force=False):
+        if force: self._get_spos(force)
         result = []
         def f(spo): result.append(spo)
-        self.iterspos(f)
+        self.iter_spos(f)
         return result
-
-    def is_ppo_discharged(self,ppoid):
-        self._getpevs() 
-        return self.pevs.isdischarged(ppoid)
-
-    def is_ppo_violated(self,ppoid):
-        self._getpevs()
-        return self.pevs.isviolation(ppoid)
-
-    def is_spo_discharged(self,spoid):
-        self._getsevs()
-        return self.sevs.isdischarged(spoid)
-
-    def is_spo_violated(self,spoid):
-        self._getsevs()
-        return self.sevs.isviolation(spoid)
-
-    def get_ppo_evidence(self,ppoid): 
-        self._getpevs()
-        return self.pevs.getevidence(ppoid)
-
-    def get_spo_evidence(self,spoid):
-        self._getsevs()
-        return self.sevs.getevidence(spoid)
 
     def get_open_ppos(self):
         result = []
         def f(ppo):
-            if not ppo.isdischarged(): result.append(ppo)
-        self.iterppos(f)
+            if not ppo.is_closed(): result.append(ppo)
+        self.iter_ppos(f)
         return result
 
     def get_violations(self):
         result = []
-        def f(pev):
-            if pev.isviolation(): result.append(pev)
-        self.iterpevs(f)
+        def f(ppo):
+            if ppo.is_violated(): result.append(ppo)
+        self.iter_ppos(f)
         return result
 
     def get_delegated(self):
         result = []
-        def f(pev):
-            if pev.isdelegated(): result.append(pev)
-        self.iterpevs(f)
+        def f(ppo):
+            if ppo.is_delegated(): result.append(ppo)
+        self.iter_ppos(f)
         return result
 
-    def _getppos(self):
+    def _get_ppos(self):
         if self.ppos is None:
-            xnode = UF.get_ppo_xnode(self.capp.path,self.cfile.getfilename(),
-                                     self.cfun.getname())
+            xnode = UF.get_ppo_xnode(self.capp.path,self.cfile.name,self.cfun.name)
             if not xnode is None:
                 self.ppos = CFunctionPPOs(self,xnode)
             else:
-                print('Unable to load ppos for ' + self.cfun.getname())
+                print('Unable to load ppos for ' + self.cfun.name)
 
-    def _getspos(self):
-        if self.spos is None:
-            xnode = UF.get_spo_xnode(self.capp.path,self.cfile.getfilename(),
-                                         self.cfun.getname())
+    def _get_spos(self,force=False):
+        if self.spos is None or force:
+            xnode = UF.get_spo_xnode(self.capp.path,self.cfile.name,self.cfun.name)
             if not xnode is None:
                 self.spos = CFunctionSPOs(self,xnode)
             else:
-                print('Unable to load spos for ' + self.cfun.getname())
+                print('Unable to load spos for ' + self.cfun.name)
 
-    def _savespos(self,cnode):
-        UF.save_spo_file(self.capp.path,self.cfile.getfilename(),
-                             self.cfun.getname(),cnode)
+    def _save_spos(self,cnode):
+        UF.save_spo_file(self.capp.path,self.cfile.name,self.cfun.name,cnode)
 
-    def _getpevs(self):
-        if self.pevs is None:
-            xnode = UF.get_pev_xnode(self.capp.path,self.cfile.getfilename(),
-                                     self.cfun.getname())
-            if not xnode is None:
-                self.pevs = CFunctionPEVs(self,xnode)
-            else:
-                print('Unable to load pevs for' + self.cfun.getname())
-
-    def _getsevs(self):
-        if self.sevs is None:
-            xnode = UF.get_sev_xnode(self.capp.path,self.cfile.getfilename(),
-                                         self.cfun.getname())
-            if not xnode is None:
-                self.sevs = CFunctionSEVs(self,xnode)
-            else:
-                print('Unable to load sevs for ' + self.cfun.getname())
-        
-        
-        
